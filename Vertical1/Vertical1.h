6fd3c5da0efd423088b50e61b6f31ad259451672
Adrian Ferreyra <luis.ferrreyra@despegar.com>
2015-07-21 12:28:34 -0300

    Initial Commit

---
+++ Vertical1/Vertical1.h
@@ -0,0 +1,19 @@
+//
+//  Vertical1.h
+//  Vertical1
+//
+//  Created by Adrian Ferreyra on 7/21/15.
+//  Copyright (c) 2015 Despegar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Vertical1.
+FOUNDATION_EXPORT double Vertical1VersionNumber;
+
+//! Project version string for Vertical1.
+FOUNDATION_EXPORT const unsigned char Vertical1VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Vertical1/PublicHeader.h>
+
+
